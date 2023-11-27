a4166090142752b7632c1a3cd837719671b51d65
geromt <gerardomartinez205@gmail.com>
2023-11-27 11:11:37 -0600

    feat: Add first version of config_reader.py

--- app/config_reader.py
+++ app/config_reader.py
@@ -1,12 +1,22 @@
 import yaml
+from enum import Enum
+
+
+class CoordinatesType(Enum):
+    PIXEL_COORDINATES = 0
+    NORM_COORDINATES = 1
+    REAL_WORLD_COORDINATES = 2
 
 
 class ConfigReader:
-    def __init__(self, config_file: str = "config.yaml"):
-        with open(config_file, 'r') as file:
-            config = yaml.safe_load(file)
-            self.tracker_config = config["config"]
-            self.output_config = config["output"]
+    def __init__(self, config_file: str = "./config.yaml"):
+        try:
+            with open(config_file, 'r') as file:
+                config = yaml.safe_load(file)
+                self.tracker_config = config["config"]
+                self.output_config = config["output"]
+        except FileNotFoundError:
+            print(f"No se encontró el archivo {config_file}")
 
     @property
     def camera_index(self):
@@ -70,3 +80,70 @@ class ConfigReader:
             return self.tracker_config["min_tracking_confidence"]
         else:
             return 0.5
+
+    @property
+    def type(self):
+        if "type" in self.output_config:
+            return self.output_config["type"]
+        else:
+            return False
+
+    @property
+    def frame_height(self):
+        if "frame_height" in self.output_config:
+            return self.output_config["frame_height"]
+        else:
+            return False
+
+    @property
+    def frame_width(self):
+        if "frame_width" in self.output_config:
+            return self.output_config["frame_width"]
+        else:
+            return False
+
+    @property
+    def flip_x(self):
+        if "flip_x" in self.output_config:
+            return self.output_config["flip_x"]
+        else:
+            return False
+
+    @property
+    def flip_y(self):
+        if "flip_y" in self.output_config:
+            return self.output_config["flip_y"]
+        else:
+            return False
+
+    @property
+    def lm_list(self):
+        if "lm_list" in self.output_config:
+            return self.output_config["lm_list"]
+        else:
+            return None
+
+    @property
+    def include_box(self):
+        if "include_box" in self.output_config:
+            return self.output_config["include_box"]
+        else:
+            return False
+
+    @property
+    def include_center(self):
+        if "include_center" in self.output_config:
+            return self.output_config["include_center"]
+        else:
+            return False
+
+    @property
+    def coordinates(self):
+        if "norm_coordinates" in self.output_config and self.output_config["norm_coordinates"]:
+            return CoordinatesType.NORM_COORDINATES
+        elif "pixel_coordinates" in self.output_config and self.output_config["pixel_coordinates"]:
+            return CoordinatesType.PIXEL_COORDINATES
+        elif "real_world_coordinates" in self.output_config and self.output_config["real_world_coordinates"]:
+            return CoordinatesType.REAL_WORLD_COORDINATES
+        else:
+            return CoordinatesType.PIXEL_COORDINATES
